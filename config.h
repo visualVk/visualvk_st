e12251e679c9651ecd69983f26c268ae7cb29b20
Tom <moonaa@visualvk.localdomain>
2021-06-02 21:42:00 +0800

    add scrollback

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Hack:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -206,6 +206,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i =  1} },
+	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
